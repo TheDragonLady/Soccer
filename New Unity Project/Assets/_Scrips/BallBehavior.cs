57cd7015560a0f47ad4dcccd30a4f175205f0a75
Christa <cmlhc99@gmail.com>
2019-08-20 11:47:34 +1200

    SoccerScripts

---
+++ New Unity Project/Assets/_Scrips/BallBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float ballThrust;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(transform.forward * ballThrust);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/_Scrips/PaddleMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMove : MonoBehaviour
+{
+
+    public float speed = 1f;
+    public float clampValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        MovePaddle(Input.GetAxis("Horizontal"));
+
+    }
+
+    public void MovePaddle(float amountToMove)
+    {
+        Vector3 clampedGameObjectPosition = gameObject.transform.position + new Vector3(0, 0, amountToMove * speed);
+        clampedGameObjectPosition.z = Mathf.Clamp(clampedGameObjectPosition.z, -clampValue, clampValue);
+
+
+        gameObject.transform.position = clampedGameObjectPosition;
+       
+    }
+}
