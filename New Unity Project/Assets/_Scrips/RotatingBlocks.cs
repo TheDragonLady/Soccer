c706292eb63591d4f971f2a83c73dfcd992b532a
Christa <cmlhc99@gmail.com>
2019-08-23 11:44:29 +1200

    RotatingBlocks

---
+++ New Unity Project/Assets/_Scrips/RotatingBlocks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBlocks : MonoBehaviour
+{
+    public float xValue;
+    public float yValue;
+    public float zValue;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xValue, yValue, zValue);   
+    }
+}
